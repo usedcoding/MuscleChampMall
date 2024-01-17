9e8ee73e5c373859351858ec75b14a0d916575e1
kael0595 <changgyu549@gmail.com>
2024-01-17 18:25:34 +0900

    SecurityConfig 작성

---
+++ src/main/java/com/example/MCM/base/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.MCM.base.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeRequests(
+            authorizeRequests -> authorizeRequests
+                .requestMatchers("/**")
+                .permitAll()
+        )
+        .csrf().ignoringRequestMatchers(
+            new AntPathRequestMatcher("/**"))
+        .and()
+        .formLogin()
+        .loginPage("/member/login")
+        .defaultSuccessUrl("/")
+        .and()
+        .logout()
+        .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+        .logoutSuccessUrl("/")
+        .invalidateHttpSession(true);
+
+    return http.build();
+  }
+
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+}
