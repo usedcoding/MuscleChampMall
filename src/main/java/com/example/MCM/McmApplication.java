a3e436118a90444cb7ea76ca49f151643f01e246
usedcoding <ja14759@gmail.com>
2024-01-17 18:12:37 +0900

    init

---
+++ src/main/java/com/example/MCM/McmApplication.java
@@ -0,0 +1,13 @@
+package com.example.MCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MCM/McmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
