588fc08fa257799e16dfc80065ec86b3533b492b
kael0595 <changgyu549@gmail.com>
2024-01-29 20:45:45 +0900

    create 작성

--- src/main/java/com/example/MCM/domain/notice/controller/NoticeController.java
+++ src/main/java/com/example/MCM/domain/notice/controller/NoticeController.java
@@ -1,14 +1,20 @@
 package com.example.MCM.domain.notice.controller;
 
+import com.example.MCM.domain.member.entity.Member;
+import com.example.MCM.domain.member.service.MemberService;
+import com.example.MCM.domain.notice.dto.NoticeDto;
 import com.example.MCM.domain.notice.entity.Notice;
 import com.example.MCM.domain.notice.service.NoticeService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -17,6 +23,8 @@ import java.util.List;
 public class NoticeController {
 
   private final NoticeService noticeService;
+
+  private final MemberService memberService;
 
   @GetMapping("/list")
   public String list(Model model) {
@@ -41,7 +49,21 @@ public class NoticeController {
   }
 
   @GetMapping("/create")
-  public String create(){
+  public String create(NoticeDto noticeDto){
     return "notice/create";
+  }
+
+  @PostMapping("/create")
+  public String create(@Valid NoticeDto noticeDto,
+                       Principal principal) {
+
+    Member author = this.memberService.getMember(principal.getName());
+
+    this.noticeService.createValidate(author);
+
+    Notice notice = this.noticeService.create(author, noticeDto);
+
+    return "redirect:/notice/list";
+
   }
 }
--- src/main/java/com/example/MCM/domain/notice/service/NoticeService.java
+++ src/main/java/com/example/MCM/domain/notice/service/NoticeService.java
@@ -1,11 +1,16 @@
 package com.example.MCM.domain.notice.service;
 
 import com.example.MCM.base.exception.DataNotFoundException.DataNotFoundException;
+import com.example.MCM.domain.member.entity.Member;
+import com.example.MCM.domain.notice.dto.NoticeDto;
 import com.example.MCM.domain.notice.entity.Notice;
 import com.example.MCM.domain.notice.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +29,25 @@ public class NoticeService {
     if (_notice.isPresent()) {
       return _notice.get();
     } throw new DataNotFoundException("notice not found");
+  }
+
+  public void createValidate(Member author) {
+
+    if (!author.getRole().equals("ADMIN")) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "공지사항 작성 권한이 없습니다.");
+    }
+  }
+
+  public Notice create(Member author, NoticeDto noticeDto) {
+
+    Notice notice = Notice.builder()
+        .subject(noticeDto.getSubject())
+        .content(noticeDto.getContent())
+        .author(author)
+        .createDate(LocalDateTime.now())
+        .build();
+
+    return this.noticeRepository.save(notice);
+
   }
 }
