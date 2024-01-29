fb05e26a56a6ca889d0f05217ae885141d1236f9
kael0595 <changgyu549@gmail.com>
2024-01-29 20:16:13 +0900

    detail 작성

--- src/main/java/com/example/MCM/domain/notice/controller/NoticeController.java
+++ src/main/java/com/example/MCM/domain/notice/controller/NoticeController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -26,5 +27,16 @@ public class NoticeController {
 
     return "notice/list";
 
+  }
+
+  @GetMapping(value = "/{id}")
+  public String detail(Model model,
+                       @PathVariable("id") Long id) {
+
+    Notice notice = this.noticeService.findById(id);
+
+    model.addAttribute("notice", notice);
+
+    return "notice/detail";
   }
 }
--- src/main/java/com/example/MCM/domain/notice/service/NoticeService.java
+++ src/main/java/com/example/MCM/domain/notice/service/NoticeService.java
@@ -1,11 +1,13 @@
 package com.example.MCM.domain.notice.service;
 
+import com.example.MCM.base.exception.DataNotFoundException.DataNotFoundException;
 import com.example.MCM.domain.notice.entity.Notice;
 import com.example.MCM.domain.notice.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +17,12 @@ public class NoticeService {
 
   public List<Notice> getAll() {
     return this.noticeRepository.findAll();
+  }
+
+  public Notice findById(Long id) {
+    Optional<Notice> _notice = this.noticeRepository.findById(id);
+    if (_notice.isPresent()) {
+      return _notice.get();
+    } throw new DataNotFoundException("notice not found");
   }
 }
