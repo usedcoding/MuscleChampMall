e85af3cb3246206c6e483da9e2bd3bfffccbfb56
kael0595 <changgyu549@gmail.com>
2024-02-05 04:59:54 +0900

    admin product 페이징 적용

--- src/main/java/com/example/MCM/domain/admin/controller/AdminController.java
+++ src/main/java/com/example/MCM/domain/admin/controller/AdminController.java
@@ -12,10 +12,14 @@ import com.example.MCM.domain.product.service.ProductService;
 import com.example.MCM.domain.review.entity.Review;
 import com.example.MCM.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.util.ArrayList;
@@ -37,15 +41,28 @@ public class AdminController {
   private final OrderService orderService;
   @GetMapping("/product")
   public String adminProduct(Model model,
-                             Principal principal){
+                             Principal principal,
+                             @RequestParam(value = "page",defaultValue = "0") int page,
+                             @RequestParam(value = "size", defaultValue = "20") int size){
 
     Member member = this.memberService.getMember(principal.getName());
 
     if (!member.getRole().equals(MemberRole.ADMIN)) return "/";
 
-    List<Product> productList = this.productService.getAll();
+    Pageable pageable = PageRequest.of(page, size);
+
+    Page<Product> productPage = this.productService.getProducts(pageable);
+
+    List<Product> productList = productPage.getContent();
+
+    long totalProducts = productPage.getTotalElements();
+
+    int totalPages = productPage.getTotalPages();
 
     model.addAttribute("productList",productList);
+    model.addAttribute("page", page);
+    model.addAttribute("totalProducts", totalProducts);
+    model.addAttribute("totalPages", totalPages);
 
     return "admin/product";
   }
